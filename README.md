dfe27f3c2045bcdb51e3a3a3fd052ce081833bd0
Trinh Duc Tran <Trinh.Duc.Tran@mgm-tp.com>
2025-02-20 10:51:59 +0700

    Add more steps in README to run, and add more log

--- README.md
+++ README.md
@@ -9,11 +9,38 @@ A simple implementation of Linear Regression.
 * Code sample in this repo is mainly from [Linear Regression from scratch (Gradient Descent)](https://www.kaggle.com/code/tentotheminus9/linear-regression-from-scratch-gradient-descent
 )
 
-# How to start
+# Python version
 ```shell
-pip install -r requirements.txt
+➜  ~ python3 --version
+Python 3.12.9
+➜  ~ pip3 --version
+pip 25.0 from /usr/local/lib/python3.12/site-packages/pip (python 3.12)
+```
 
-python main.py 
+# How to start
+Create a virtual environment
+```shell
+python3 -m venv myenv
+```
+
+Activate the virtual environment
+```shell
+source myenv/bin/activate
+```
+
+Install Dependencies
+```shell
+pip3 install -r requirements.txt
+```
+
+Run
+```shell
+python3 main.py 
+```
+
+Deactivate the virtual environment
+```shell
+deactivate
 ```
 
 Then open an `animation.gif` file.
--- main.py
+++ main.py
@@ -3,57 +3,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Load data
+# Load training dataset
 data = pd.read_csv('./train.csv')
 
-# Plot data
+# Visualize 1000 houses
 NUMBER_OF_HOUSES = 1000
 plt.plot(data['GrLivArea'][:NUMBER_OF_HOUSES], data['SalePrice'][:NUMBER_OF_HOUSES], 'ro')
-plt.title('Sale Price vs Living Area')
+plt.title('Sale Price vs Living Area (automatically closed in few seconds)')
 plt.xlabel('Ground Living Area')
 plt.ylabel('Sale Price')
-plt.show()
+plt.show(block=False)
+plt.pause(5)  # Wait for few seconds
+plt.close()
 
-# Gradient descent
+# Gradient descent constants
 LEARNING_RATE = 0.01
-MAX_NUMBER_OF_ITERATIONS = 2000
-GR_LIV_AREA = data['GrLivArea']
-SALE_PRICE = data['SalePrice']
-n = SALE_PRICE.size
+MAX_NUMBER_OF_ITERATIONS = 1000
+
+gr_liv_area = data['GrLivArea']
+sale_price = data['SalePrice']
+m = sale_price.size
+
+# Pick some random theta values to start with
 np.random.seed(123)
-# Pick some random THETA values to start with
-THETA = np.random.rand(2)
+initial_theta = np.random.rand(2)
 
 # Standardization and add a bias term
-GR_LIV_AREA = (GR_LIV_AREA - GR_LIV_AREA.mean()) / GR_LIV_AREA.std()
-GR_LIV_AREA = np.c_[np.ones(GR_LIV_AREA.shape[0]), GR_LIV_AREA]
-
+gr_liv_area = (gr_liv_area - gr_liv_area.mean()) / gr_liv_area.std()
+gr_liv_area = np.c_[np.ones(gr_liv_area.shape[0]), gr_liv_area]
 
 def gradient_descent(gr_liv_area, sale_price, theta, iterations, learning_rate):
     past_costs = []
     past_thetas = [theta]
     for i in range(iterations):
         sale_price_predict = np.dot(gr_liv_area, theta)
-        error = sale_price_predict - sale_price
-        cost = 1 / (2 * n) * np.dot(error.T, error)
-        past_costs.append(cost)
-        theta = theta - (learning_rate * (1 / n) * np.dot(gr_liv_area.T, error))
+        cost = sale_price_predict - sale_price
+        total_cost = 1 / (2 * m) * np.dot(cost.T, cost)
+        past_costs.append(total_cost)
+        theta = theta - (learning_rate * (1 / m) * np.dot(gr_liv_area.T, cost))
         past_thetas.append(theta)
 
     return past_thetas, past_costs
 
 
-past_thetas, past_costs = gradient_descent(GR_LIV_AREA, SALE_PRICE, THETA, MAX_NUMBER_OF_ITERATIONS, LEARNING_RATE)
-THETA = past_thetas[-1]
+past_thetas, past_costs = gradient_descent(gr_liv_area, sale_price, initial_theta, MAX_NUMBER_OF_ITERATIONS, LEARNING_RATE)
+optimal_theta = past_thetas[-1]
 
-print("Gradient Descent: {:.2f}, {:.2f}".format(THETA[0], THETA[1]))
+print("Theta 0 and theta 1: {:.2f}, {:.2f}".format(optimal_theta[0], optimal_theta[1]))
 
-# Plot the cost function
-plt.title('Cost Function')
+# Visualize the cost function
+plt.title('Cost Function (automatically closed in few seconds)')
 plt.xlabel('No. of iterations')
 plt.ylabel('Cost')
 plt.plot(past_costs)
-plt.show()
+plt.show(block=False)
+plt.pause(5)  # Wait for few seconds
+plt.close()
 
 ##### Animation
 
@@ -63,12 +68,11 @@ ax = plt.axes()
 plt.title('Sale Price vs Living Area')
 plt.xlabel('Ground Living Area')
 plt.ylabel('Sale Price')
-plt.scatter(GR_LIV_AREA[:, 1], SALE_PRICE, color='red')
+plt.scatter(gr_liv_area[:, 1], sale_price, color='red')
 line, = ax.plot([], [], lw=2)
 annotation = ax.text(-1, 700000, '')
 annotation.set_animated(True)
 plt.close()
-
 
 # Generate the animation data
 def init():
@@ -85,6 +89,7 @@ def animate(i):
     annotation.set_text('Cost = %.2f e10' % (past_costs[i] / 10000000000))
     return line, annotation
 
-
+print("Generating animation...")
 anim = animation.FuncAnimation(fig, animate, init_func=init, frames=300, interval=0, blit=True)
 anim.save('animation.gif', writer='pillow', fps=30)
+print("Done!")
